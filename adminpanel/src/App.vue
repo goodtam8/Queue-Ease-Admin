07591eff496bad7bc1d9411a958563d23a19ccc8
goodtam8 <21225133@life.hkbu.edu.hk>
2024-07-16 17:14:41 +0800

    implement a v-if for the log out button and reformat the layout

--- adminpanel/src/App.vue
+++ adminpanel/src/App.vue
@@ -1,19 +1,73 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<template>
+    <div id="app">
+        <header>
+            <meta charset="utf-8">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
+    
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+              <div class="container-fluid">
+            <a class="navbar-brand" href="#">UniTrack</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Teacher</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Student</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " aria-disabled="false">Course</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Analytic</a>
+                    </li>
+                    <form class="d-flex " role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
 
+
+                </ul>
+
+            </div>
+        </div>
+        <div v-if="hasToken">
+                <button type="button" class="btn btn-primary my-4" @click="logout">Log Out</button>
+            </div>
+    </nav>
+            
+        </header>
+    
+    </div>
+    
+    <RouterView/>
+</template>
+
+<script setup>
+import { computed } from 'vue';
 
 const logout = function() {
-  localStorage.removeItem('token');
-  location.reload()
+    localStorage.removeItem('token');
+    location.reload();
 }
 
+const hasToken = computed(() => {
+    return localStorage.getItem('token') !== null;
+});
 </script>
 
-<template>
-  <header>
-    <button type="button" class="btn btn-primary my-4" @click="logout">Log Out</button>
-  </header>
-
-  <RouterView />
-</template>
+<style>
+* {
+  padding: 0px;
+  margin: 0px;
+  box-sizing: border-box;
+}
+</style>
--- adminpanel/src/views/LoginView.vue
+++ adminpanel/src/views/LoginView.vue
@@ -1,54 +1,53 @@
-<script setup>
-// imports
-import { ref } from 'vue';
-
-// credentials
-const credentials = ref({
+<template>
+      <form class="container my-5">
+        <div class="mb-3 ">
+          <label for="exampleInputEmail1" class="form-label">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" v-model="credentials.email">
+          <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" v-model="credentials.password">
+        </div>
+        
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  
+  const credentials = ref({
     email: '',
     password: ''
-});
-
-// methods
-const login = async () => {
+  });
+  
+  const login = async () => {
     try {
-        // fetch
-        const response = await fetch('/api/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(credentials.value)
-        });
-
-        // response
-        const data = await response.json();
-
-        if (!response.ok) {
-            throw new Error(data.message);
-        }
-
-        // save token to local storage
-        localStorage.setItem('token', data.token);
+      const response = await fetch('/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials.value)
+      });
+  
+      const data = await response.json();
+  
+      if (!response.ok) {
+        throw new Error(data.message);
+      }
+  
+      localStorage.setItem('token', data.token);
     } catch (error) {
-        alert(error);
+      alert(error);
     }
-}
-</script>
-
-<template>
-    <main>
-        <form class="container my-5" @submit.prevent="login">
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" v-model="credentials.email">
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" v-model="credentials.password">
-            </div>
-           
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-    </main>
-</template>
+  }
+  </script>
+  
+  <style>
+    main {
+      height: 100vh; /* Set the height of the main container to full viewport height */
+    }
+  </style>
+  
