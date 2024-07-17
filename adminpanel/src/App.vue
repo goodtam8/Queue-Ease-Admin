fd70c932aa62dc951f4cbf21d8e1340a1bbb0b07
goodtam8 <21225133@life.hkbu.edu.hk>
2024-07-17 14:28:15 +0800

    update some customize setting and make use of token in creatcourse

--- adminpanel/src/App.vue
+++ adminpanel/src/App.vue
@@ -8,11 +8,11 @@
             <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="#">UniTrack</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" v-if="hasToken">
+                        <span class="navbar-toggler-icon" ></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav" v-if="hasToken">
+                    <div class="collapse navbar-collapse" id="navbarNav" v-if="hasToken">
+                        <ul class="navbar-nav">
                             
                                 <li v-if="$route.name === 'home'">
                                     <router-link to="/" class="nav-link active">Home</router-link>
