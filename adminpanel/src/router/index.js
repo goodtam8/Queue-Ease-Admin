6e64753be67a922303178af3870a63847201eb7c
goodtam8 <21225133@life.hkbu.edu.hk>
2024-07-18 14:43:42 +0800

    implement the course display and pagination

--- adminpanel/src/router/index.js
+++ adminpanel/src/router/index.js
@@ -50,6 +50,14 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/TeacherAssignView.vue')
     },
+    {
+      path: '/course',
+      name: 'course',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/Course.vue')
+    },
   ]
 })
 
---
+++ adminpanel/src/views/Course.vue
@@ -0,0 +1,137 @@
+<template>
+    <div>
+        <br>
+
+        <nav aria-label="breadcrumb">
+    
+    <ol class="breadcrumb">
+
+        <li class="breadcrumb-item">
+
+            <router-link to="/" >Home</router-link>
+
+        </li>
+
+        
+
+        <li class="breadcrumb-item active" aria-current="page">Course</li>
+
+
+
+    </ol>
+
+</nav>
+
+    <br>
+        <div class="row col-12 col-md-8 ">
+    
+    
+    
+    
+    
+            <div class="col" v-for="c in course " :key="c">
+    
+                <div class="card" style="width: 18rem;">
+    
+                    <div class="card-body">
+    
+    
+    
+                        <h5 class="card-title">{{ c._id }}</h5>
+                        <p class="card-text">{{ c.location }}</p>
+                        <router-link :to="'/update/'+c._id" class="btn btn-primary" id="submitBtn">
+                            Edit
+                            </router-link>
+    
+    
+    
+                    </div>
+    
+                </div>
+                <br>
+    
+            </div>
+    
+        </div>
+
+     <nav aria-label="Page navigation example">
+        <ul class="pagination" id="pagination" >
+            <div v-for="pg in numbers " :key="pg">
+            <li class="page-item active" aria-current="page" v-if="pg===page">
+                <a class="page-link" v-on:click="setpage(pg)">{{ pg }}</a>
+            </li>
+            <li class="page-item" aria-current="page" v-else>
+                <a class="page-link" v-on:click="setpage(pg)">{{ pg }}</a>
+            </li>
+
+        </div>
+        </ul>
+    </nav>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    </div>
+</template>
+
+
+
+<script setup>
+import { useRoute, useRouter } from "vue-router";
+import { ref, onMounted, computed, watch, nextTick } from 'vue'
+
+onMounted(async () => {
+    // if there is an id in the route
+   
+        getCourse();
+    
+});
+const page=ref(1);
+const perpage=ref(6);
+const total=ref(1);
+const totalpage=ref(1);
+
+
+const course = ref([])
+
+// A function to fetch a booking
+async function getCourse() {
+    const params = [
+    `page=${page.value}`
+]
+// fetch the booking
+const response = await fetch(`/api/course?${params}`);
+// convert the response to json
+const json = await response.json();
+// return the json
+course.value = json.courses;
+page.value=json.page;
+perpage.value=json.perPage;
+total.value=json.total
+totalpage.value=total.value/perpage.value;
+
+
+
+}
+async function setpage(abc){
+    page.value=abc;
+
+}
+
+const numbers = computed(() => {
+  return Array.from({ length: totalpage.value+1 }, (_, index) => index+1);
+});
+watch(() => page.value, () => {
+    getCourse();
+});
+
+
+</script>
